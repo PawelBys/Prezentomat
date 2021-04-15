f2bf5c100dc49579e5dbc743f73429683ffb7297
PawelBys <kontakt.pawel.bys@gmail.com>
2021-04-15 14:49:42 +0200

    zabawy na początku z bazą

--- Models/GatheringClass.cs
+++ Models/GatheringClass.cs
@@ -11,7 +11,8 @@ namespace Prezentomat.Models
     [Table("gathering", Schema = "public")]
     public class GatheringClass
     {
-        [Key]
+        [Key] // to wskazuje na to, która zmienna jest kluczem głównym
+        public int gathering_id { get; set; }
         public int current_amount { get; set; }
         public int target_amount { get; set; }
         public DateTime finish_date { get; set; }
