da6c4c8d2890b678e80e3b3e646865465547932e
Piotr Kasprzyk <piotr02468@gmail.com>
2021-04-15 18:38:39 +0200

    dodalem obsluge logowania i rejestracji bez współpracy z bazą danych

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Prezentomat.Models;
 using Prezentomat.DataContext;
-using System.Data.SqlClient;
+using System;
 
 namespace Prezentomat.Controllers
 {
     public class UserController : Controller
     {
-        ApplicationDbContext _context;
+        ApplicationDbContext _context=new ApplicationDbContext();
+        
 
         public UserController()
         {
@@ -31,9 +30,37 @@ namespace Prezentomat.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Login(UserClass uss)
+        {
+            var email = "";
+            var password = "";
+            try
+            {
+                email = _context.UserDetails.Where(p => p.email == uss.email).Single().email;
+                password = _context.UserDetails.Where(p => p.password == uss.password).Single().password;
+            }catch(Exception e){;}
+            if (!email.Equals("")&&!password.Equals(""))
+            {
+                return View("UserView");
+            }
+            else
+            {
+                return View("Regist");
+            }
+        }
+
         public ActionResult Regist()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Regist(UserClass uss)
+        {
+            //należy zrobic tu zapis danych do bazy
+
+            return View("UserView");
         }
 
 
