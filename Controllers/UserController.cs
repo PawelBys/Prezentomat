a97eec0c114aa82425f9e7f5de525162cec0b53c
PawelBys <kontakt.pawel.bys@gmail.com>
2021-04-19 09:57:49 +0200

    zrobione wylogowanie - sesja

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -35,7 +35,7 @@ namespace Prezentomat.Controllers
             return View();
         }
 
-        //niby dziala poprawnie ale nie otwiera sie user view bo tam jest blad 
+        
         [HttpPost]
         public ActionResult Login([Bind(Include = "email,password")] UserClass userClass)
         {
@@ -80,6 +80,13 @@ namespace Prezentomat.Controllers
             }
 
             return View(userClass);
+        }
+
+        public ActionResult Logout()
+        {
+            Session["UserID"] = null;
+
+            return RedirectToAction("Login");
         }
 
 
